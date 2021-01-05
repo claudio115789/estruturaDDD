ebdc11f7807438230e7bbcd13fa497a1f526c1a8
Claudio Ribeiro <claudio115789@outlook.com>
2021-01-05 14:57:57 -0300

    Create ContextBase.cs

---
+++ Domain/Infrastructure/Config/ContextBase.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Infrastructure.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Config
+{
+    public  class ContextBase : DbContext
+    {
+        public ContextBase() : base("ConnectionString")
+        {
+
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBluilder)
+        {
+            base.OnModelCreating(modelBluilder);
+            modelBluilder.Configurations.Add(new ProdutoMap());
+        }
+
+        public DbSet<Produto> ObjetoProduto { get; set; }
+    }
+}
