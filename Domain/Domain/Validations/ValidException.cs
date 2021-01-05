5954960630ac134f7223194a62a97a2ab45e7f48
Claudio Ribeiro <claudio115789@outlook.com>
2021-01-05 14:56:23 -0300

    Create ValidException.cs

---
+++ Domain/Domain/Validations/ValidException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Validations
+{
+   public class ValidException: Exception
+    {
+        public  IList<ViolacaoValid> _Erros = new List<ViolacaoValid>();
+        public readonly Expression<Func<object, object>> _objeto = x => x;
+
+
+        protected void AdicionarMensagemErro(string Mensagem)
+        {
+            _Erros.Add(new ViolacaoValid { Propriedade = _objeto, Mensagem = Mensagem });
+        }
+    }
+}
