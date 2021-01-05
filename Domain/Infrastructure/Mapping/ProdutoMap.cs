9e209b67f7b01a70630e671ab2724a6d14db249b
Claudio Ribeiro <claudio115789@outlook.com>
2021-01-05 14:57:43 -0300

    Create ProdutoMap.cs

---
+++ Domain/Infrastructure/Mapping/ProdutoMap.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Mapping
+{
+     public class ProdutoMap : EntityTypeConfiguration<Produto>
+
+    {
+        public ProdutoMap()
+        {
+            HasKey(t => t.Id);
+            Property(t => t.NomeProduto).HasMaxLength(150);
+            Property(t => t.Preco);
+
+        }
+    }
+}
