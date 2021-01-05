cb82593e88266995b050f6d23c649c9c45755a8a
Claudio Ribeiro <claudio115789@outlook.com>
2021-01-05 15:01:49 -0300

    Create ProdutoRepository.cs

---
+++ Domain/Infrastructure/RepositoryProduto/ProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Domain.Interface.InterfaceProduto;
+using Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.RepositoryProduto
+{
+     public class ProdutoRepository : RepositoryGeneric<Produto>,IProdutoRepository
+    {
+    }
+}
