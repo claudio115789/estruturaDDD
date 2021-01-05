3d05bb7572344e3572b07eb4df6fa51592562011
Claudio Ribeiro <claudio115789@outlook.com>
2021-01-05 14:55:17 -0300

    Create IProdutoRepository.cs

---
+++ Domain/Domain/Interface/InterfaceProduto/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Domain.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface.InterfaceProduto
+{
+    public interface IProdutoRepository : InterfaceGeneric<Produto>
+    {
+    }
+}
