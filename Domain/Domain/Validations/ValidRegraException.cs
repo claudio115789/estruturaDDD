472e855a4d89cfed27b3056cb725ba67bbee808e
Claudio Ribeiro <claudio115789@outlook.com>
2021-01-05 15:10:20 -0300

    Create ValidRegraException.cs

---
+++ Domain/Domain/Validations/ValidRegraException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Validations
+{
+   public  class ValidRegraException<T>: ValidException
+
+    {
+        internal void AdicionarErroPara<TPropriedade>(Expression<Func<T,TPropriedade>>propriedade, string mensagem)
+
+        {
+            _Erros.Add(new ViolacaoValid { Propriedade = propriedade, Mensagem = mensagem });
+        }
+    }
+}
