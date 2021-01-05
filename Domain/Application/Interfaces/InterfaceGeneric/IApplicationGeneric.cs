b330e0aeb989904b0f46eee427300ee139cce994
Claudio Ribeiro <claudio115789@outlook.com>
2021-01-05 14:53:05 -0300

    Create IApplicationGeneric.cs

---
+++ Domain/Application/Interfaces/InterfaceGeneric/IApplicationGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.InterfaceGeneric
+{
+   public  interface IApplicationGeneric<T>where T : class
+    {
+        void Adicionar(T objeto);
+        void Atualizar(T objeto);
+        void Deletar(int Id);
+        T Obter(int Id);
+        List<T> ListarTodos();
+    }
+}
