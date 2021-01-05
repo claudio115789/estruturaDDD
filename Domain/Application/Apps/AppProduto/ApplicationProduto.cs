b151dd2c6a899e5c690ebee0c8ec3a6b254cd4ff
Claudio Ribeiro <claudio115789@outlook.com>
2021-01-05 14:56:34 -0300

    Create ApplicationProduto.cs

---
+++ Domain/Application/Apps/AppProduto/ApplicationProduto.cs
@@ -0,0 +1,47 @@
+﻿using Application.Interfaces.InterfaceGeneric;
+using Domain.Entities;
+using Domain.Interface.InterfaceProduto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Apps.AppProduto
+{
+    public class ApplicationProduto : IApplicationGeneric<Produto>
+    {
+        IProdutoRepository Repository;
+
+        public ApplicationProduto(IProdutoRepository _IprodutoRepository)
+        {
+
+            Repository = _IprodutoRepository;
+        }
+
+        public void Adicionar(Produto Objeto)
+        {
+            Repository.Adicionar(Objeto);
+        }
+
+        public void Atualizar(Produto Objeto)
+        {
+            Repository.Atualizar(Objeto);
+        }
+
+        public void Deletar(int Id)
+        {
+            Repository.Deletar(Id);
+        }
+
+        public List<Produto> ListarTodos()
+        {
+            return Repository.ListarTodos();
+        }
+
+        public Produto Obter(int Id)
+        {
+            return Repository.Obter(Id);
+        }
+    }
+}
