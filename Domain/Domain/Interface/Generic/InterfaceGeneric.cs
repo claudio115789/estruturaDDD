24ce68419c127921ad26b0c54ff3d7994ec43d42
Claudio Ribeiro <claudio115789@outlook.com>
2021-01-05 14:55:03 -0300

    Create InterfaceGeneric.cs

---
+++ Domain/Domain/Interface/Generic/InterfaceGeneric.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Generic
+{
+    public interface InterfaceGeneric<T> where T : class
+    {
+        void Adicionar(T Objeto);
+        void Atualizar(T Objeto);
+        void Deletar(int Id);
+        T Obter(int Id);
+        List<T> ListarTodos();
+
+    }
+}
